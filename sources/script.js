bc0a7485487062d50d69cb74819fbc6e65c5d537
Your Name <you@example.com>
2014-02-07 13:13:31 +0100

    script.js

--- sources/script.js
+++ sources/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function()
 {
-	init_btn_login();
+	init_btns_login(); // Initialisation des boutons connexion / Deconnexion
 });
+
+function init_btns_login ()
+{
+	init_btn_login ();
+	init_btn_logoff ();
+}
 
 function init_btn_login ()
 {
@@ -20,6 +26,23 @@ function init_btn_login ()
 		{
 			//alert(res);
 			$("#connexion").html(res);
+			init_btns_login ();
 		});
 	});
 }
+
+function init_btn_logoff ()
+{
+	$('#btn-deconnexion').click(function(e)
+	{		
+		e.preventDefault();	
+		var options={"url":"index.php?page=deco"}
+
+		$.ajax(options).done(function(res)
+		{
+			$("#connexion").html(res);
+			init_btns_login ();
+		});
+	});
+}
+
