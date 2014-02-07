481a3567e1c830cf6055430d8c432a18dc278497
Your name <you@example.com>
2014-02-07 12:48:09 +0100

    maj

--- controllers/login.php
+++ controllers/login.php
@@ -1,9 +1,11 @@
 <?php 
 
-
-	
-	if (isset($_SESSION["login"]))
+	$_SESSION['login']=$_POST['login'];
+	var_dump($_POST);
+	$_SESSION['login']="toto";
+	if (isset($_SESSION["login"]) || $_SESSION['login']!="")
 		{
+
 			require "views/logged.html";
 		}
 		
