3b1fb1cdff20b2086ee0009a204a49da8f9c5773
Your Name <you@exemple.com>
2014-02-10 12:27:04 +0100

    update base SQL

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function init_session()
 	$_SESSION['created']=true;
 }
 
-$db = mysqli_connect('127.0.0.1','root','troiswa','forum');
+$db = mysqli_connect('127.0.0.1','root','coucou','forum');
 
 if ($db === false)
 	die("erreur de connexion à la base MySQL");
