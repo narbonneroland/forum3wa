f1cdf19309d5740f9f64bff32eef7c1e2c3f6fc6
Your name <you@example.com>
2014-02-11 09:22:22 +0100

    create sujet

--- controllers/form.php
+++ controllers/form.php
@@ -1,22 +1,23 @@
 <?php
-	if ($_GET["objet"] == "showform")
-		require "views/formSujetAjout.html";
-	
-	else if ($_POST["objet"] == "sujetaddform")
-	{	
-		$data = array();
-		$data[] = $_POST["titre"];
-		$data[] = $_POST["description"];
-		$sujet = $manager->CreateSujet($data);
-		$save = $sujet->Save();
-	}
 
-	else if ($_POST["objet"] == "sujetmodifform")
-	{	
-		$sujet = $manager->getSujet($id);
-		$titre = $sujet->getTitre();
-		$titre = $sujet->getDescription();
-	}
+	if(isset($_GET["objet"]))
+	{
+		if ($_GET["objet"] == "showform")
+			require "views/formSujetAjout.html";
 		
+		else if ($_GET["objet"] == "sujetaddform")
+		{	
+			$data = array();
+			$sujet = $manager->CreateSujet($_POST);
+			$save = $sujet->Save();
+		}
+
+		else if ($_GET["objet"] == "sujetmodifform")
+		{	
+			$sujet = $manager->getSujet($id);
+			$titre = $sujet->getTitre();
+			$titre = $sujet->getDescription();
+		}
+	}	
 
 ?>
